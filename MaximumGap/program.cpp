9e1058303432e6e3b426ab08e84585c713dc78ea
Robin <spdf@live.com>
2015-05-07 19:48:43 +0800

    164 Maximum Gap(uncomplete)

---
+++ MaximumGap/program.cpp
@@ -0,0 +1,24 @@
+#include "../include/prevector.h"
+
+
+#define ABS(x) ((x ^ x >> 31) - (x >> 31)) //For *32bit int*  
+#define MAX(a, b) (a > b ? a : b)
+int maximumGap(vector<int>& nums) 
+{
+    int size = nums.size();
+    if (size < 2) return 0;
+    vector<int> gaps;
+    for (int i = 1; i != size; i++)
+    {
+        gaps.push_back(ABS(nums[i] - nums[i-1]));
+    }
+    int max = 0;
+    for (auto i : gaps)
+        max = MAX(max, i);
+    return max;
+}
+void Mymain()
+{
+    vector<int> test = {1, 2, 8, 97, 85, 8};
+    cout << maximumGap(test) << endl;
+}
