336e6e2f69e7596800e1667725f6709488d227d4
Robin <spdf@live.com>
2015-07-01 21:59:10 +0800

    finish KthLargestElem and add some util functions

--- KthLargestElem/program.cpp
+++ KthLargestElem/program.cpp
@@ -1,13 +1,33 @@
 #include "../include/prevector.h"
+#include "../include/presort.h"
 
-const auto quickSortCore = [](std::vector<int>& nums, size_t s, size_t e)->size_t{
-    if (e - s == 1){
-        if (nums[s] > nums[e])
-            std::swap(nums[s], nums[e]);
-        return 0;
+//e - s >= 3
+//too slow
+inline void putMiddleToEnd(std::vector<int>& nums, size_t s, size_t e)
+{
+    auto it_b = nums.begin();
+    std::vector<decltype(it_b)> a = {it_b + s, it_b + std::floor((e - s) / 2 + 0.5), it_b + e};
+    int i = 0, min_s = 0, max_s = 0;
+    while (i++ != 2){
+        if (a[i][0] < a[min_s][0]) min_s = i;
+        if (a[i][0] > a[max_s][0]) max_s = i;
     }
+    i = 0;
+    while(i != 2){
+        if (i != min_s && i != max_s) break;
+        i++;
+    }
+    std::swap(a[i][0], a[2][0]);
+}
+
+inline void twoSort(std::vector<int>& nums, size_t s){
+    if (nums[s] > nums[s + 1]) std::swap(nums[s], nums[s + 1]);
+}
+
+inline size_t quickSortCore(std::vector<int>& nums, size_t s, size_t e)
+{
+    //putMiddleToEnd(nums, s, e);
     auto mid = nums[e];
-    cout << s << ", " << e << ":" << mid << endl;
     size_t i = s, j = s;
     while (j <= e) {
         if (nums[j] <= mid){
@@ -18,38 +38,58 @@ const auto quickSortCore = [](std::vector<int>& nums, size_t s, size_t e)->size_
         j++;
     }
     return i - 1;
-};
+}
 
 void quickSort(std::vector<int>& nums, size_t s, size_t e)
 {
-    PrintVector(nums);
     auto m = quickSortCore(nums, s, e);
-    cout << "===" << m << "===" << endl;
-    if (m < s || m > e) return;
-    if (m - s > 1) quickSort(nums, s, m - 1);
-    if (e - m > 1) quickSort(nums, m + 1, e);
+    //if (m < s || m > e) return;
+    if (m - s > 2) quickSort(nums, s, m - 1);
+    else if (m - s == 2) twoSort(nums, s);
+    if (e - m > 2) quickSort(nums, m + 1, e);
+    else if (e - m == 2) twoSort(nums, m + 1);
 }
 
-int quickSortKMin(std::vector<int>& nums, size_t s, size_t e, int k)
+void startQuickSort(std::vector<int>& nums, size_t s, size_t e)
 {
-    auto m = quickSortCore(nums, s, e);
-    if (m < s || m > e) return;
-    if (m == k) return nums[k];
-    if (m < k) quickSortKMin(nums, m + 1, e, k - m);
-
+    if (s == e) return;
+    if (e - s == 1) {
+        twoSort(nums, s);
+        return;
+    }
+    quickSort(nums, s, e);
 }
 
 int findKthLargest(vector<int>& nums, int k)
 {
-    int num = nums.size();
-
-    return num;
+    //convert to k min's index
+    k = nums.size() - k;
+    size_t s = 0,  e = nums.size() - 1;
+    auto m = quickSortCore(nums, s, e);
+    while (k != m) {
+        s = (k < m) ? s : m + 1;
+        e = (k < m) ? m - 1 : e;
+        m = quickSortCore(nums, s, e);
+    }
+    return nums[k];
 }
+
 int Mymain()
 {
-    //std::vector<int> n {67, 12, 2, 3, 56, 98, 91, 54};
-    std::vector<int> n {3, 9, 2, 7, 1, 5, 8};
-    quickSort(n, 0, n.size());
-    PrintVector(n);
+    std::vector<int> nums;
+    GenerateRandomArray(nums, 10001);
+    auto nums_s = nums;
+    int k = 1539;
+    int myk = 0, stdk = 2;
+    {TIC
+        //startQuickSort(nums, 0, nums.size() - 1);
+        myk = findKthLargest(nums, k);
+    TOC}
+    {TIC
+        std::sort(nums_s.begin(), nums_s.end());
+        stdk = nums_s[nums.size() - k];
+    TOC}
+    cout << "Check increase : " << std::boolalpha << CheckSorted(nums, nIncrease) << std::noboolalpha << endl;
+    cout << "Check K Largest : " << std::boolalpha << (myk == stdk) << std::noboolalpha << endl;
     return 0;
 }
