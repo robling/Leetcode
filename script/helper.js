d33a52e732c484232c15a9d407e47c83c2c8b072
Robin.L <spdf@live.com>
2015-08-27 01:40:20 +0800

    Add 'update' commond 2.add helper page 3.arrange 'new' and 'solve' commond

--- script/helper.js
+++ script/helper.js
@@ -14,30 +14,49 @@ const leetcode_uri = {
 const repo_root_path = path.resolve(__dirname, '../');
 const list_data_file = path.resolve(repo_root_path, './problems.json');
 
-program.version('1.0.0');
+program.version('0.1.0');
 program
-    .command('new <problem_num>') //add new solution to this repository
-    .action(function(problem_num){
+    .command('init')
+    .description('initialize the repo with /src and problems.json(data and cache)')
+    .action(function(){
+        newcachefile();
+    });
+program
+    .command('new <problem_num> [dir_name]')
+    .description('add new solution to this repository. a dir with specific name will be create')
+    .action(function(problem_num, dir_name){
+        console.log('Problem Number is : %j', problem_num);
+        console.log('Problem dir name is : ', optional_dir_name | '__default__');
+    });
+program
+    .command('solve <problem_num> [solution_code]')
+    .description('add solving infomations to specific problems with (or not) solution uri code')
+    .action(function(problem_num, solution_code){
         console.log('Problem Number is : %j', problem_num);
     });
 program
-    .command('solve <problem_num>') //add solving infomations to
-    .action(function(problem_num){
-        console.log('Problem Number is : %j', problem_num);
-    });
-program
-    .command('list') //list all solutions that has been submited
+    .command('list')
+    .description('list all solutions that has been solved')
     .action(function(){
 
     });
 program
-    .command('debug') //list all solutions that has been submited
+    .command('debug')
+    .description('debug enterpoint, do not use it')
     .action(function(){
-        var data = fs.readFileSync(path.resolve(repo_root_path, './test/test.html'),"utf-8");
-        //leetcode_html_parser_all(data);
-        newcachefile();
+        update_cache();
+    });
+program
+    .command('*')
+    .action(function(){
+        console.error('unknown commond');
+        program.help();
     });
 program.parse(process.argv);
+
+function make_dir_name(str) {
+    return str.replace(/ /, '_');
+}
 
 function parser_detail(str) {
     let get_uri = function(){
@@ -61,17 +80,9 @@ function parser_detail(str) {
     return data;
 }
 
-function leetcode_html_parser(leetcode_html, num){
-    //const reg_str = `<tr>.*<td>.*</td>.*<td>${ num }</td>.*<td>.*</td>.*</tr>`;
-    const reg_str = `<td>${ num }</td>(\n.*?)*?</tr>`;
-    console.log(reg_str);
-    let reg = new RegExp(reg_str, 'g');
-    let rst = reg.exec(leetcode_html);
-    return rst;
-}
-
-function leetcode_html_parser_all(leetcode_htmls){
-    let num = 1;
+function leetcode_html_parser_all(leetcode_htmls, last_index){
+    let offset = last_index | 1;
+    let num = last_index | 1;
     let rst = [];
     let parser_helper = function(index){
         console.log(num);
@@ -82,7 +93,7 @@ function leetcode_html_parser_all(leetcode_htmls){
             let reg = new RegExp('\\d+');
             //console.log(reg.exec(rst_single));
             //console.log(rst_single[0]);
-            rst[num] = rst_single[0];
+            rst[num - offset] = rst_single[0];
             num++;
             return true;
         }
@@ -108,10 +119,17 @@ function leetcode_html_parser_all(leetcode_htmls){
 }
 
 function get_html_all() {
-    let algorithms_html = request('GET', leetcode_uri.algorithms ).body;
-    let database_html   = request('GET', leetcode_uri.database   ).body;
-    let shell_html      = request('GET', leetcode_uri.shell      ).body;
-    return [algorithms_html, database_html, shell_html];
+    console.log('Getting leetcode problems page...');
+    try {
+        let algorithms_html = request('GET', leetcode_uri.algorithms ).body;
+        let database_html   = request('GET', leetcode_uri.database   ).body;
+        let shell_html      = request('GET', leetcode_uri.shell      ).body;
+        return [algorithms_html, database_html, shell_html];
+    } catch (e) {
+        console.error('Getting leetcode problems page Failed');
+        console.error(e);
+        process.exit(1);
+    }
 }
 
 function newcachefile() {
@@ -122,8 +140,39 @@ function newcachefile() {
     data.last_index = problems_meta.length;
     data.problems = {};
     for (let x in problems_meta){
-        data.problems[x] = parser_detail(problems_meta[x]);
+        data.problems[parseInt(x) + 1] = parser_detail(problems_meta[x]);
+        data.problems[parseInt(x) + 1].is_solved = false;
     }
     fs.writeFileSync(list_data_file,JSON.stringify(data, null, 4));
 }
+
+function update_cache(){
+    if (!fs.existsSync(list_data_file)) {
+        newcachefile();
+        return true;;
+    }
+    else {
+        let data = JSON.parse(fs.readFileSync(list_data_file, 'utf-8'));
+        data.last_update = new Date();
+        let offset = data.last_index + 1;
+        //test error
+        if (data['problems'][data.last_index] === null || typeof data['problems'][offset] !== 'undefined'){
+            console.error('Cache file has error, please check and fix it.');
+            console.log('last_index:', data.last_index);
+            console.log(data['problems'][data.last_index]);
+            console.log('next_index:', offset);
+            console.log(data['problems'][offset]);
+            console.log('detail: last_index does not equal to solutions listed');
+            return;
+        }
+        let problems_meta = leetcode_html_parser_all(get_html_all(), offset);
+        for (let x in problems_meta){
+            data.problems[parseInt(x) + offset] = parser_detail(problems_meta[x]);
+            data.problems[parseInt(x) + offset].is_solved = false;
+        }
+        data.last_index += problems_meta.length;
+        fs.writeFileSync(list_data_file,JSON.stringify(data, null, 4));
+    }
+}
+
 })();
