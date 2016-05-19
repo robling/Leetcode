b09bf899dbf959a69a94b91e0ddfd5fd80464624
Robin Liu <spdf@live.com>
2016-05-19 15:30:52 +0800

    get ready for typescript development

--- script/helper.ts
+++ script/helper.ts
@@ -2,7 +2,10 @@
  Aim: 
     0. refactor with typescript
     1. split helper functions to subfolder
-    2.  
+    2. using await to replace *-sync module
+    3. support other languages
+    4. ahead version to 2.0
  */
 let k : string = 'Hello Warld!';
 console.log(k);
+const dirname : string = __dirname;
